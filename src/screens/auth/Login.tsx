9b2d7edb8e46c7e41115795008892a27c26b9b59
nguyenduc2710 <hongducnguyenho0@gmail.com>
2024-09-14 11:28:58 +0700

    update app logo

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Image, Linking, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Dimensions, Image, Linking, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { TextInput, MD3Colors, Text as PaperTxt, Checkbox } from "react-native-paper";
 import { COLORS, FONT_COLORS } from "../../assets/styles/variables";
 import { DividerWithTxt } from "../../components/global/DividerWithTxt";
@@ -13,6 +13,8 @@ import { RootState } from "../../redux/Store";
 import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { usePushNotifications } from "../../../usePushNotification";
+
+const { width } = Dimensions.get('window');
 
 const Login: React.FC = () => {
     const [email, setEmail] = useState("");
@@ -75,7 +77,7 @@ const Login: React.FC = () => {
     }, []);
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <View style={styles.header}>
                 <Image
                     source={require('../../assets/imgs/logo_cut-removebg-preview.png')}
@@ -90,105 +92,116 @@ const Login: React.FC = () => {
                 </Text>
                 <Text style={styles.blurTxt}>Hello there, login to continue</Text>
             </View>
-            <View style={styles.body}>
-                <TextInput
-                    mode="outlined"
-                    label='Email'
-                    placeholder="Email"
-                    outlineStyle={focusInput === 'email'
-                        ? styles.outlineInputFocus
-                        : styles.defaultOutline}
-                    onFocus={() => setFocusInput('email')}
-                    onBlur={() => setFocusInput(undefined)}
-                    style={[styles.input, { marginTop: 10, backgroundColor: "#FFF" }]}
-                    theme={{
-                        colors: {
-                            primary: COLORS.skyBlue,
-                            text: FONT_COLORS.greyFontColor,
-                        },
-                    }}
-                    onChangeText={val => setEmail(val)}
-                />
-                <TextInput
-                    mode="outlined"
-                    label='Password'
-                    placeholder="Password"
-                    secureTextEntry={secureTextEntry}
-                    style={[styles.input, { backgroundColor: "#FFF" }]}
-                    outlineStyle={
-                        focusInput === 'password'
+            <View style={{ flex: 1 }}>
+                <View style={styles.body}>
+                    <TextInput
+                        mode="outlined"
+                        label='Email'
+                        placeholder="Email"
+                        outlineStyle={focusInput === 'email'
                             ? styles.outlineInputFocus
-                            : styles.defaultOutline
-                    }
-                    onFocus={() => setFocusInput('password')}
-                    onBlur={() => setFocusInput(undefined)}
-                    theme={{
-                        colors: {
-                            primary: COLORS.skyBlue,
-                            text: FONT_COLORS.greyFontColor
+                            : styles.defaultOutline}
+                        onFocus={() => setFocusInput('email')}
+                        onBlur={() => setFocusInput(undefined)}
+                        style={[styles.input, { marginTop: 10, backgroundColor: "#FFF" }]}
+                        theme={{
+                            colors: {
+                                primary: COLORS.skyBlue,
+                                text: FONT_COLORS.greyFontColor,
+                            },
+                        }}
+                        onChangeText={val => setEmail(val)}
+                    />
+                    <TextInput
+                        mode="outlined"
+                        label='Password'
+                        placeholder="Password"
+                        secureTextEntry={secureTextEntry}
+                        style={[styles.input, { backgroundColor: "#FFF" }]}
+                        outlineStyle={
+                            focusInput === 'password'
+                                ? styles.outlineInputFocus
+                                : styles.defaultOutline
                         }
-                    }}
-                    onChangeText={val => setPassword(val)}
-                    right={
-                        <TextInput.Icon
-                            icon={secureTextEntry ? 'eye-off-outline' : 'eye-outline'}
-                            color={MD3Colors.primary0}
-                            onPress={() => setSecureTextEntry(!secureTextEntry)}
-                        />
-                    }
-                />
-                <View style={styles.moreActionLogin}>
-                    <View style={GLOBAL_STYLES.horizontalBetweenCenter}>
-                        <Checkbox
-                            status={isRemember ? 'checked' : 'unchecked'}
-                            onPress={() => {
-                                setIsRemember(!isRemember);
+                        onFocus={() => setFocusInput('password')}
+                        onBlur={() => setFocusInput(undefined)}
+                        theme={{
+                            colors: {
+                                primary: COLORS.skyBlue,
+                                text: FONT_COLORS.greyFontColor
+                            }
+                        }}
+                        onChangeText={val => setPassword(val)}
+                        right={
+                            <TextInput.Icon
+                                icon={secureTextEntry ? 'eye-off-outline' : 'eye-outline'}
+                                color={MD3Colors.primary0}
+                                onPress={() => setSecureTextEntry(!secureTextEntry)}
+                            />
+                        }
+                    />
+                    <View style={styles.moreActionLogin}>
+                        <View style={GLOBAL_STYLES.horizontalBetweenCenter}>
+                            <Checkbox
+                                status={isRemember ? 'checked' : 'unchecked'}
+                                onPress={() => {
+                                    setIsRemember(!isRemember);
+                                }}
+                                color="#2563EB"
+                            />
+                            <Text>Remember me</Text>
+                        </View>
+                        <TouchableOpacity
+                            style={{
+                                marginVertical: 20,
                             }}
-                            color="#2563EB"
-                        />
-                        <Text>Remember me</Text>
+                            onPress={() => { navigation.navigate('FORGOT_PASSWORD') }}
+                        >
+                            <PaperTxt style={styles.forgotPassTxt}>Forgot Password?</PaperTxt>
+                        </TouchableOpacity>
                     </View>
                     <TouchableOpacity
-                        style={{
-                            marginVertical: 20,
-                        }}
-                        onPress={() => { navigation.navigate('FORGOT_PASSWORD') }}
+                        activeOpacity={0.6}
+                        onPress={() => handleLogin()}
                     >
-                        <PaperTxt style={styles.forgotPassTxt}>Forgot Password?</PaperTxt>
+                        <CustomBtn text="Login" />
                     </TouchableOpacity>
+                    {/*  */}
+                    {/* <DividerWithTxt customStyle={styles.divider} color={FONT_COLORS.blurFontColor} text="Or continue with Google account" />
+
+                    <TouchableOpacity>
+                        <CustomBtn
+                            text="Google"
+                            key={'ggBtn'}
+                            customStyle={styles.ggBtn}
+                            icon={
+                                <Image style={{ width: 30, height: 30, marginRight: 7 }} source={{ uri: 'https://cdn-icons-png.freepik.com/512/281/281764.png?ga=GA1.1.1218810189.1708404630' }} />
+                            }
+                            colorTxt={{ color: FONT_COLORS.greyFontColor }}
+                        />
+                    </TouchableOpacity> */}
                 </View>
-                <TouchableOpacity
-                    activeOpacity={0.6}
-                    onPress={() => handleLogin()}
-                >
-                    <CustomBtn text="Login" />
-                </TouchableOpacity>
-
-                <DividerWithTxt customStyle={styles.divider} color={FONT_COLORS.blurFontColor} text="Or continue with Google account" />
-
-                <TouchableOpacity>
-                    <CustomBtn
-                        text="Google"
-                        key={'ggBtn'}
-                        customStyle={styles.ggBtn}
-                        icon={
-                            <Image style={{ width: 30, height: 30, marginRight: 7 }} source={{ uri: 'https://cdn-icons-png.freepik.com/512/281/281764.png?ga=GA1.1.1218810189.1708404630' }} />
-                        }
-                        colorTxt={{ color: FONT_COLORS.greyFontColor }}
-                    />
-                </TouchableOpacity>
+                <View style={styles.footerCtn}>
+                    <View>
+                        <Image
+                            style={{ width: width - 40, height: 60 }}
+                            source={require('../../assets/imgs/logo_sider.png')}
+                            resizeMode="contain"
+                        />
+                    </View>
+                    <View style={GLOBAL_STYLES.horizontalBetweenCenter}>
+                        <PaperTxt>
+                            Didn't have an account? {' '}
+                        </PaperTxt>
+                        <TouchableOpacity style={{ display: 'flex', alignItems: 'center' }}
+                            onPress={() => handleSendEmail()}
+                        >
+                            <PaperTxt style={{ color: COLORS.skyBase, textAlignVertical: 'center' }}>Contact Us!</PaperTxt>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
-            <View style={styles.footerCtn}>
-                <PaperTxt>
-                    Didn't have an account? {' '}
-                </PaperTxt>
-                <TouchableOpacity style={{ display: 'flex', alignItems: 'center' }}
-                    onPress={() => handleSendEmail()}
-                >
-                    <PaperTxt style={{ color: COLORS.skyBase, textAlignVertical: 'center' }}>Contact Us!</PaperTxt>
-                </TouchableOpacity>
-            </View>
-        </View>
+        </ScrollView>
     );
 };
 
@@ -196,7 +209,7 @@ export default Login;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 2,
         paddingHorizontal: 20,
         paddingVertical: 15,
         backgroundColor: '#FFF'
@@ -218,6 +231,7 @@ const styles = StyleSheet.create({
     body: {
         flex: 1,
         justifyContent: 'flex-start',
+        marginBottom: 120,
     },
     input: {
         marginTop: 10,
@@ -254,10 +268,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFF'
     },
     footerCtn: {
-        display: 'flex',
-        flexDirection: 'row',
+        // flex: 1,
+        marginBottom: 20,
         justifyContent: 'center',
         alignItems: 'center',
-        height: 'auto',
     },
 });   
